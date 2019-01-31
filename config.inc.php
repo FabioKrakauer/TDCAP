1c0c2c0d8de821b44fbbb8448dc23ac2abd97f9c
Fabio Krakauer <comvocefabio@gmail.com>
2019-01-31 15:20:20 -0200

    add config inicial

---
+++ config.inc.php
@@ -0,0 +1,16 @@
+<?php
+
+//Configurações do banco de dados!
+$host     = "localhost";
+$dbname = "tdccap_prework";
+$username = "root";
+$password   = "";
+$port = 3306;
+
+//Definição dos nomes para acesso
+define("APP_ROOT", dirname(__FILE__));
+define("APP_NAME", "TDC");
+define("APP_URL", "localhost/TDCAP");
+
+ini_set("upload_max_filesize", "100M");
+
