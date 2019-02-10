27b51e4a2c8e8c7e48d7dab1f997c4b005ea3ee2
Gabriel Nader <gabrielgnader@gmail.com>
2019-02-10 19:08:02 -0200

    Companies complete

--- js/Views/companyView.js
+++ js/Views/companyView.js
@@ -1,17 +1,63 @@
+addCompanyView = function(){
+    let content = 
+        `
+            <h1 class="h3 text-white text-center p-1 mb-4">Adicionar Empresa</h1>
+            <form action="../controller/newCompany.php" method="post">
+                <div class="container row mx-auto">
+                    <div class="form-group col-12">
+                        <label for="name">Nome:</label>
+                        <input type="text" name="name" id="name" class="form-control" placeholder="Nome da empresa">
+                    </div>
+                    <div class="form-group col-12">
+                        <label for="cnpj">CNPJ:</label>
+                        <input type="text" name="cnpj" id="cnpj" class="form-control" placeholder="CNPJ da empresa">
+                    </div>
+                    <div class="form-group col-12">
+                        <label for="adress">Endereço:</label>
+                        <input type="text" name="adress" id="adress" class="form-control" placeholder="Endereço da empresa">
+                    </div>
+                    <div class="form-group col-12">
+                        <label for="telephone">Telefone:</label>
+                        <input type="text" name="telephone" id="telephone" class="form-control" placeholder="Telefone da empresa">
+                    </div>
+                    <div class="form-group col-12">
+                        <label for="site">Site:</label>
+                        <input type="text" name="site" id="site" class="form-control" placeholder="Site da empresa">
+                    </div>
+                    <div class="form-group col-12">
+                        <label for="contact">Contato do responsável:</label>
+                        <input type="text" name="contact" id="contact" class="form-control" placeholder="Contato do responsável">
+                    </div>
+                    <div class="form-group col-12">
+                        <label for="email">E-mail:</label>
+                        <input type="email" name="email" id="email" class="form-control" placeholder="E-mail do responsável">
+                    </div>
+                    <div class="form-group col-12">
+                        <input type="submit" name="action" class="btn save text-white" value="Salvar">
+                    </div>
+                </div>
+            </form>
+        `
+
+    $('#dynamic-content').html(content)
+}
+
 createCompanyView = function (data) {
     var listCompanies = ``
 
     data.forEach(function (data) {
-        listCompanies += `
-                            <li class='list-group-item'>
-                                ${data.name}
-                                <button class='btn btn-outline-primary btn-sm btn-lg-lg float-right' onclick="editCompany(${data.id})">
-                                    Editar
-                                </button>
-                            </li>`
+        listCompanies += 
+        `
+            <li class='list-group-item'>
+                ${data.name}
+                <button class='btn btn-outline-primary btn-sm btn-lg-lg float-right' onclick="editCompany(${data.id})">
+                    Editar
+                </button>
+            </li>
+        `
     })
 
-    var empresasCadastradas = `
+    var content = `
         <h1 class='h3 text-white text-center p-1 mb-4'>Empresas Cadastradas</h1>
         <div class='container'><ul class='container list-group list-group-flush'>
             <ul>
@@ -20,7 +66,7 @@ createCompanyView = function (data) {
         </div>
     `
 
-    $('#dynamic-content').html(empresasCadastradas)
+    $('#dynamic-content').html(content)
 }
 
 editCompanyView = function(data) {
