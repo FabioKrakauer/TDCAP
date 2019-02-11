93a28119766d19cbd1b92562d687f1c77e876c1e
Fábio Krakauer <comvocefabio@gmail.com>
2019-02-11 18:12:31 -0200

    add course sql

--- config.inc.php
+++ config.inc.php
@@ -4,13 +4,13 @@
 $host = "localhost";
 $dbname = "tdc";
 $username = "root";
-$password = "root";
-$port = 8888;
+$password = "";
+$port = 3306;
 
 //Definição dos nomes para acesso
 define("APP_ROOT", dirname(__FILE__));
-define("APP_NAME", "TDCAP");
-define("APP_URL", "localhost/TDCAP");
+define("APP_NAME", "tdc");
+define("APP_URL", "localhost/tdc");
 
 //Tamanho maximo para upload de arquivos
 ini_set("upload_max_filesize", "100M");
