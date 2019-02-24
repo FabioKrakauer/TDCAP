5d635610d7520966299c93920a9fd0992b9eac0c
Gabriel Nader <gabrielgnader@gmail.com>
2019-02-24 10:25:49 -0300

    Fix error on login

--- admin/login.php
+++ admin/login.php
@@ -40,69 +40,6 @@
         </form>
     </div>
 
-
-
-
-            <form action="../controller/newStudent.php" method="post">
-                <div class="row mx-auto">
-                    <div class="form-group col-12">
-                        <label for="name">Nome:</label>
-                        <input type="text" name="name" id="name" class="form-control" value="${studentData.name}">
-                    </div>
-                    <div class="form-group col-12">
-                        <label for="email">E-mail:</label>
-                        <input type="email" name="email" id="email" class="form-control" value="${studentData.email}">
-                    </div>
-                    <div class="form-group col-12">
-                        <label for="student-company">Empresa:</label>
-                        <select class="custom-select" id="student-company" required>
-                            ${companyToList}
-                        </select>
-                    </div>
-                    <div class="form-group col-12">
-                        <label for="password">Senha:</label>
-                        <input type="password" name="password" id="password" class="form-control" placeholder="Senha">
-                    </div>
-                    <div class="form-group col-12">
-                        <label for="password-conf">Confirme a senha:</label>
-                        <input type="password" name="password-conf" id="password-conf" class="form-control" placeholder="Confirme a senha">
-                    </div>
-                    <input type="hidden" value="${studentData.id}" name="studentId">
-                    <div class="form-group col-12">
-                        <input type="submit" name="action" class="btn btn-sm save text-white" value="Salvar">
-                    </div>
-                </div>
-            </form>
-            <h3 class="h4 text-white text-center p-1 mb-4">Cursos</h3>
-            <ul class="list-group list-group-flush mb-4">
-                ${courseHasList}
-            </ul>
-            <form action="../controller/addCourseToStudent.php" method="post">
-                <input type="hidden" value="${studentData.id}" name="studentId">
-                <input type="hidden" value="${courseData.id}" name="courseId">
-                <div class="row mx-auto">
-                    <div class="form-group col-12">
-                        <label for="student-course">Adicionar curso:</label>
-                        <select class="custom-select" id="student-course" required>
-                            <option value="" selected disabled>Curso...</option>
-                            ${courseToList}
-                        </select>
-                    </div>
-                    <div class="form-group col-12">
-                        <input type="submit" name="action" class="btn btn-sm save text-white" value="Salvar">
-                    </div>
-                </div>
-            </form>
-
-
-
-
-
-
-
-
-
-
     <script src="http://code.jquery.com/jquery-3.3.1.js" integrity="sha256-2Kok7MbOyxpgUVvAk/HJ2jigOSYS2auK4Pfzbm7uH60=" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
