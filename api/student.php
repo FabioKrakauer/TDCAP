e2281db5b70a2ffe824cd72e3229d0c9691ee2ea
Fabio Krakauer <comvocefabio@gmail.com>
2019-02-26 16:38:09 -0300

    add completed courses on student api

--- api/student.php
+++ api/student.php
@@ -13,17 +13,23 @@
             $field = $database->getFieldsValues("SELECT `id` FROM `user`");
             foreach($field as $row){
                 $user = new User($row["id"]);
+                $userID = $row["id"];
                 $array = [
                     "id" => $user->getID(),
                     "name" => $user->getName(),
                     "email" => $user->getEmail(),
                     "course" => array(),
                     "company" => $user->getCompany()->getID(),
+                    "completed_courses" => array(),
                     "first_access" => $user->getFirstAccess(),
                 ];
                 foreach($user->getCourses() as $course){
                     $id = $course->getID();
                     array_push($array["course"], $id);
+                }
+                $completededCourses = $database->getFieldsValues("SELECT `course_id` FROM `user_exam_result` WHERE `user_id`='$userID'");
+                foreach($completededCourses as $completed){
+                    array_push($array["completed_courses"], $completed["course_id"]);
                 }
                 array_push($result, $array);
             
@@ -34,17 +40,23 @@
         }else{
             $field = $database->getFieldValue("SELECT `id` FROM `user` WHERE `id`='$id'");
             $user = new User($field["id"]);
+            $userID = $field["id"];
                 $result = [
                     "id" => $user->getID(),
                     "name" => $user->getName(),
                     "email" => $user->getEmail(),
                     "course" => array(),
+                    "completed_courses" => array(),
                     "company" => $user->getCompany()->getID(),
                     "first_access" => $user->getFirstAccess(),
                 ];
                 foreach($user->getCourses() as $course){
                     $id = $course->getID();
                     array_push($result["course"], $id);
+                }
+                $completededCourses = $database->getFieldsValues("SELECT `course_id` FROM `user_exam_result` WHERE `user_id`='$userID'");
+                foreach($completededCourses as $completed){
+                    array_push($result["completed_courses"], $completed["course_id"]);
                 }
                 
             $json = json_encode($result, JSON_UNESCAPED_UNICODE);
