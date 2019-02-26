6d7b70d967683aa4f78c210848c6c72ab2ad2353
Fabio Krakauer <comvocefabio@gmail.com>
2019-02-26 16:25:59 -0300

    add edit student

---
+++ controller/editStudent.php
@@ -0,0 +1,22 @@
+<?php
+
+require_once '../config.inc.php';
+
+if(isset($_POST["action"])){
+    $id = $_POST["studentId"];
+    $name = $_POST["name"];
+    $email = $_POST["email"];
+    $company = $_POST["studentCompany"];
+    $password = $_POST["password"];
+    $password_confirmation = $_POST["password-conf"];
+
+    if($password != $password_confirmation){
+        echo "Suas senhas não conferem!";        
+        die();
+    }
+    $password = md5($password);
+    global $database;
+
+    $database->query("UPDATE `user` SET `name`='$name',`email`='$email',`password`='$password',`company`='$company' WHERE `id`='$id'");
+    echo "Aluno editado!";
+}
