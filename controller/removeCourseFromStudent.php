b830fed2d2c9dba25976be5162715ae01f3091c1
Fábio Krakauer <comvocefabio@gmail.com>
2019-03-04 16:19:56 -0300

    add remove course from student

---
+++ controller/removeCourseFromStudent.php
@@ -0,0 +1,13 @@
+<?php
+
+require_once '../config.inc.php';
+
+global $database;
+
+if(isset($_POST["action"])){
+    $user = $_POST["studentId"];
+    $course = $_POST["courseId"];
+    $database->query("DELETE FROM `user_course` WHERE `user_id`='$user' AND `course_id`='$course'");
+    $_SESSION["message-user"] = "0;Você removeu o curso (#$course) do aluno (#$user) com sucesso!";
+    header("Location: ../admin/");
+}
