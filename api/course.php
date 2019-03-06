e2fc1039193aee7f287580f12f31bf8b61e27b95
Fabio Krakauer <comvocefabio@gmail.com>
2019-03-06 16:27:32 -0300

    add questions to course api

--- api/course.php
+++ api/course.php
@@ -21,11 +21,24 @@
                     "slides" => array(),
                     "inicial_slide" => $course->getInicialSlide()->getID(),
                     "end_slide" => $course->getEndSlide()->getID(),
+                    "questions" => array(),
                 ];
                 $courseReallyID = $course->getID();
                 $slides = $database->getFieldsValues("SELECT `id` FROM `slides` WHERE `course_id`='$courseReallyID'");
                 foreach($slides as $slide){
                     array_push($array["slides"], $slide["id"]);
+                }
+                $questions = $database->getFieldsValues("SELECT * FROM `course_test` WHERE `course_id`='$courseReallyID'");
+                foreach($questions as $question){
+                    $questionArray = [
+                        "title" => $question["question"],
+                        "1-alternative" => $question["1_alternative"],
+                        "2-alternative" => $question["2_alternative"],
+                        "3-alternative" => $question["3_alternative"],
+                        "4-alternative" => $question["4_alternative"],
+                        "correct_alternative" => $question["correct_alternative"]
+                    ];
+                    array_push($array["questions"], $questionArray);
                 }
                 array_push($result, $array);
             }
@@ -42,11 +55,24 @@
                     "company" => $course->getCompany()->getID(),
                     "inicial_slide" => $course->getInicialSlide()->getID(),
                     "end_slide" => $course->getEndSlide()->getID(),
+                    "questions" => array(),
                 ];
                 $courseReallyID = $course->getID();
                 $slides = $database->getFieldsValues("SELECT `id` FROM `slides` WHERE `course_id`='$courseReallyID'");
                 foreach($slides as $slide){
                     array_push($result["slides"], $slide["id"]);
+                }
+                $questions = $database->getFieldsValues("SELECT * FROM `course_test` WHERE `course_id`='$courseReallyID'");
+                foreach($questions as $question){
+                    $questionArray = [
+                        "title" => $question["question"],
+                        "1-alternative" => $question["1_alternative"],
+                        "2-alternative" => $question["2_alternative"],
+                        "3-alternative" => $question["3_alternative"],
+                        "4-alternative" => $question["4_alternative"],
+                        "correct_alternative" => $question["correct_alternative"]
+                    ];
+                    array_push($result["questions"], $questionArray);
                 }
             $json = json_encode($result, JSON_UNESCAPED_UNICODE);
             echo $json;
