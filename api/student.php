a052252d53868f4a65f4b081a01d413fc4e10dcc
Fábio Krakauer <comvocefabio@gmail.com>
2019-03-15 21:43:37 -0300

    add student courses categorys

--- api/student.php
+++ api/student.php
@@ -21,15 +21,32 @@
                     "course" => array(),
                     "company" => $user->getCompany()->getID(),
                     "completed_courses" => array(),
+                    "inProgressCourses" => array(),
+                    "notViewed" => array(),
+                    "disabledCourses" => array(),
                     "first_access" => $user->getFirstAccess(),
                 ];
-                foreach($user->getCourses() as $course){
-                    $id = $course->getID();
+                $userID = $user->getID();
+                $coursesNotDisabled = $database->getFieldsValues("SELECT `course_id` FROM `user_course` WHERE `disabled`='0' AND `user_id` = '$userID'");
+                foreach($coursesNotDisabled as $course){
+                    $id = $course["course_id"];
                     array_push($array["course"], $id);
+                    if($user->getCourseProgress($id) == 0){
+                        $array["notViewed"][$id] = $user->getCourseProgress($id);
+                    }else{
+                        $completed = $database->getFieldValue("SELECT `course_id`, `result` FROM `user_exam_result` WHERE `user_id`='$userID' AND `course_id` = '$id'");
+                        if($completed == null){
+                            $array["inProgressCourses"][$id] = $user->getCourseProgress($id);
+                        }
+                    }
                 }
                 $completededCourses = $database->getFieldsValues("SELECT `course_id`, `result` FROM `user_exam_result` WHERE `user_id`='$userID'");
                 foreach($completededCourses as $completed){
                     $array["completed_courses"][$completed["course_id"]] = $completed["result"];
+                }
+                $disabledCourses = $database->getFieldsValues("SELECT `course_id` FROM `user_course` WHERE `user_id` = '$userID' AND `disabled`='1'");
+                foreach($disabledCourses as $course){
+                    array_push($array["disabledCourses"], $course["course_id"]);
                 }
                 array_push($result, $array);
             
@@ -47,12 +64,28 @@
                     "email" => $user->getEmail(),
                     "course" => array(),
                     "completed_courses" => array(),
+                    "inProgressCourses" => array(),
+                    "notViewed" => array(),
+                    "disabledCourses" => array(),
                     "company" => $user->getCompany()->getID(),
                     "first_access" => $user->getFirstAccess(),
                 ];
-                foreach($user->getCourses() as $course){
-                    $id = $course->getID();
+                $coursesNotDisabled = $database->getFieldsValues("SELECT `course_id` FROM `user_course` WHERE `disabled`='0' AND `user_id` = '$userID'");
+                foreach($coursesNotDisabled as $course){
+                    $id = $course["course_id"];
                     array_push($result["course"], $id);
+                    if($user->getCourseProgress($id) == 0){
+                        $result["notViewed"][$id] = $user->getCourseProgress($id);
+                    }else{
+                        $completed = $database->getFieldValue("SELECT `course_id`, `result` FROM `user_exam_result` WHERE `user_id`='$userID' AND `course_id` = '$id'");
+                        if($completed == null){
+                            $result["inProgressCourses"][$id] = $user->getCourseProgress($id);
+                        }
+                    }
+                }
+                $disabledCourses = $database->getFieldsValues("SELECT `course_id` FROM `user_course` WHERE `user_id` = '$userID' AND `disabled`='1'");
+                foreach($disabledCourses as $course){
+                    array_push($result["disabledCourses"], $course["course_id"]);
                 }
                 $completededCourses = $database->getFieldsValues("SELECT `course_id`, `result` FROM `user_exam_result` WHERE `user_id`='$userID'");
                 foreach($completededCourses as $completed){
