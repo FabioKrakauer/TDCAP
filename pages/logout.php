70cdddbf835352a50f2dd43cdde183bb902e8b4f
Fábio Krakauer <comvocefabio@gmail.com>
2019-03-30 09:58:46 -0300

    fix logout bug

--- pages/logout.php
+++ pages/logout.php
@@ -7,7 +7,7 @@ Auth::logout();
 
 if(isset($_GET["page"])){
     if($_GET["page"] == 0){
-        header("Location: admin/login.php");
+        header("Location: ../admin/login.php");
         die();
     }else{
         header("Location: login.php");
